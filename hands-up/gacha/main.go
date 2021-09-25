b9cd85a0b6f63c7eb99957ecfc4a9acc7ffa112c
Jakey08 <yuweb1105@gmail.com>
2021-09-25 21:41:57 +0900

    added two opptions which is 1 gacha or 11 times consecutively

--- hands-up/gacha/main.go
+++ hands-up/gacha/main.go
@@ -10,7 +10,30 @@ func main() {
 
 	rand.Seed(time.Now().Unix()) //rand.Seed is not included in 'for loop'.
 
-	for i := 1; i < 12; i++ {
+	var n int // the repeating times of gacha
+	fmt.Println("1: 単発ガチャ 2: 11連ガチャ")
+
+LOOP:
+	//infinite loop
+	for {
+
+		fmt.Print(">")
+		var kind int
+		fmt.Scanln(&kind)
+		//Users can choose 1 or 2. When it's done, it'll break LOOP.
+		switch kind {
+		case 1: //one gacha
+			n = 1
+			break LOOP
+		case 2: //11 times
+			n = 11
+			break LOOP
+		default:
+			fmt.Println("もう一度入力してください")
+		}
+	}
+
+	for i := 1; i <= n; i++ {
 
 		num := rand.Intn(100)
 		fmt.Printf("%d回目", i)
@@ -18,7 +41,7 @@ func main() {
 		switch {
 		case num < 80:
 			fmt.Println("ノーマル")
-		case num < 94:
+		case num < 95:
 			fmt.Println("R")
 		case num < 99:
 			fmt.Println("SR")
