c7e815f091b0be79065eff745b146a51675935c4
Jakey08 <yuweb1105@gmail.com>
2021-09-25 19:13:12 +0900

    added hands-up

---
+++ hands-up/section1/step06/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().Unix())
+	num := rand.Intn(100)
+
+	// TODO: 変数numが0〜79のときは"ノーマル"、
+	// 80〜94のときは"R"、95〜98のときは"SR"、
+	// それ以外のときは"XR"と表示する
+
+	switch {
+	case num < 80:
+		fmt.Println("ノーマル")
+	case num < 94:
+		fmt.Println("R")
+	case num < 99:
+		fmt.Println("SR")
+	default:
+		fmt.Println("XR")
+	}
+
+}
