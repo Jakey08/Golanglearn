a524b86efbffb41da3687dfc65a329e92e45d685
Jakey08 <yuweb1105@gmail.com>
2021-09-29 00:54:30 +0900

    added slice array

--- hands-up/gacha/main.go
+++ hands-up/gacha/main.go
@@ -11,43 +11,58 @@ func main() {
 	rand.Seed(time.Now().Unix()) //rand.Seed is not included in 'for loop'.
 
 	var n int // the repeating times of gacha
-	fmt.Println("1: 単発ガチャ 2: 11連ガチャ")
+	// 	fmt.Println("1: 単発ガチャ 2: 11連ガチャ")
 
-LOOP:
-	//infinite loop
 	for {
+		fmt.Print("何回引きますか？>")
+		fmt.Scanln(&n)
 
-		fmt.Print(">")
-		var kind int
-		fmt.Scanln(&kind)
-		//Users can choose 1 or 2. When it's done, it'll break LOOP.
-		switch kind {
-		case 1: //one gacha
-			n = 1
-			break LOOP
-		case 2: //11 times
-			n = 11
-			break LOOP
-		default:
-			fmt.Println("もう一度入力してください")
+		if n > 0 {
+			break
 		}
+		fmt.Println("もう一度入力してください")
 	}
 
-	for i := 1; i <= n; i++ {
+	// LOOP:
+	// 	//infinite loop
+	// 	for {
 
+	// 		fmt.Print(">")
+	// 		var kind int
+	// 		fmt.Scanln(&kind)
+	// 		//Users can choose 1 or 2. When it's done, it'll break LOOP.
+	// 		switch kind {
+	// 		case 1: //one gacha
+	// 			n = 1
+	// 			break LOOP
+	// 		case 2: //11 times
+	// 			n = 11
+	// 			break LOOP
+	// 		default:
+	// 			fmt.Println("もう一度入力してください")
+	// 		}
+	// 	}
+
+	result := make([]string, n)
+	//setup like this 'make([]type length, capacity)'
+
+	for i := 0; i < len(result); i++ {
+		//the condition of for looop is i < len(result)
 		num := rand.Intn(100)
-		fmt.Printf("%d回目", i)
+		//fmt.Printf("%d回目", i)
 
 		switch {
 		case num < 80:
-			fmt.Println("ノーマル")
+			result[i] = "ノーマル"
 		case num < 95:
-			fmt.Println("R")
+			result[i] = "R"
 		case num < 99:
-			fmt.Println("SR")
+			result[i] = "SR"
 		default:
-			fmt.Println("XR")
+			result[i] = "XR"
 		}
 	}
+
+	fmt.Println(result)
 
 }
