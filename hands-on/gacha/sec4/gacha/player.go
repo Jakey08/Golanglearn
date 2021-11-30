2f4e7db54aa79b875039b7bff9f52f5fb49e5549
Jakey08 <yuweb1105@gmail.com>
2021-11-30 19:55:32 +0900

    create NewPlayer_func to return pointer of tickets and coins

--- hands-on/gacha/sec4/gacha/player.go
+++ hands-on/gacha/sec4/gacha/player.go
@@ -2,7 +2,7 @@ package gacha
 
 import "fmt"
 
-type player struct {
+type Player struct {
 	tickets int // ガチャ券の枚数
 	coin    int // コイン
 }
@@ -11,21 +11,25 @@ type player struct {
 // それぞれをフィールドに設定したPlayer型の値を生成し、
 // そのポインタを返すNewPlayer関数を作る
 
-func NewPlayer(int *player.tickets, int *player.coin) {
-	return 
+func NewPlayer(tickets, coin int) *Player {
+	//ポインタを返すから&をつける
+	return &Player{tickets: tickets, coin: coin} 
+
 }
 
+
+// TODO: メソッドをエクスポートする
 // プレイヤーが行えるガチャの回数
-func (p *player) drawableNum() int {
+func (p *Player) DrawableNum() int {
 
 // TODO: ガチャが行える回数を返す
 // ガチャ券は1枚で1回、コインは10枚で1回ガチャが行える
-return p.tickets + p.coin/10
+	return p.tickets + p.coin/10
 }
 
-func (p *player) draw(n int) {
+func (p *Player) draw(n int) {
 
-	if p.drawableNum() < n {
+	if p.DrawableNum() < n {
 	 fmt.Println("ガチャ券またはコインが不足しています")
 	 return
 	}
